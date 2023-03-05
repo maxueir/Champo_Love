eddc20d7734dbd8f019b8bed3456525eebfcb348
maxueir <123549175+maxueir@users.noreply.github.com>
2023-03-05 22:12:47 +0100

    v0.2 rotation et debut d'animation

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,6 +3,8 @@ package application;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Cursor;
+import javafx.scene.ImageCursor;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
@@ -13,16 +15,23 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane Panel = new BorderPane();
-		    BorderPane recherche_profil = new Recherche_profil();
-			
+		    BorderPane recherche_profil = new Recherche_profil(); 
+			Panel.setId("panel");
+			recherche_profil.setId("recherche_profil");
 			Panel.setCenter(recherche_profil);
+			
 			Scene scene = new Scene(Panel,500,500);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			//Panel.setId("pane");
+			
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("ChampoLove");
 			Image img = new Image("file:images/icone.jpg");
 			primaryStage.getIcons().add(img);
+			
+			Image curseur=new Image("file:images/flechedecoupee.png");
+			Cursor c = ImageCursor.chooseBestCursor(new Image[] {curseur}, 0, 0);
+			scene.setCursor(c);
+			
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
--- src/application/Recherche_profil.java
+++ src/application/Recherche_profil.java
@@ -7,13 +7,20 @@ import java.awt.Point;
 import java.awt.geom.Point2D;
 import java.util.Vector;
 
+import javafx.animation.RotateTransition;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.EventHandler;
+import javafx.geometry.Point3D;
 import javafx.stage.Stage;
+import javafx.util.Duration;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.MouseDragEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundImage;
@@ -26,38 +33,86 @@ import javafx.scene.transform.Rotate;
 
 public class Recherche_profil extends BorderPane {
 	Image imagecourante;
-	
+	Boolean dragged=false;
+	int coordsX=-1;
 	
 	public Recherche_profil() {
-		imagecourante=new Image("file:images/premier_profil.jpg");
-		ImageView imageView = new ImageView(imagecourante);
+		
+		Image imagecourante=new Image("file:images/premier_profil.jpg");
+		/*imageView = new ImageView(imagecourante);
 		imageView.setX(0); 
 	    imageView.setY(0);
-	    imageView.setFitHeight(500); 
-	    imageView.setFitWidth(500);
-	    this.getChildren().add(imageView);
-	    imageView.setOnMouseDragged(new EventHandler<MouseEvent>() {
+	    
+	    this.getChildren().add(imageView);*/
+	    this.setOnMouseDragged(new EventHandler<MouseEvent>() {
 	        @Override
 	        public void handle(MouseEvent event) {
-	        	int a=10;
+	        	//System.out.println(((Recherche_profil)event.getSource()).coordsX);
+	        	((Recherche_profil)event.getSource()).dragged=true;
+	        	/*int a=10;
 	        	int b=(int) Math.sqrt(((250 - event.getX())*(250 - event.getX())) + ((500 - event.getY())*(500 - event.getY())));
 	        	int c=(int) Math.sqrt(((250 - event.getX())*(250 - event.getX())) + ((490 - event.getY())*(490 - event.getY())));
 	        	int angle= (int)Math.acos((a*a + b*b - c*c)/(2*a*b));
-	        	//if(event.get)
-	        	imageView.getTransforms().add(new Rotate(-angle,250,500));
+	        	
+	        	
+	        	System.out.println(((Node) event.getSource()));
+	        	
+	        	
+	        	((Node) event.getSource()).getTransforms().add(new Rotate(-angle,(((BorderPane)event.getSource()).getWidth())/2,((BorderPane)event.getSource()).getHeight()));
+	        	*/
+	        	int angle =(int)(event.getSceneX()-((Recherche_profil)event.getSource()).coordsX);
+	        	//System.out.println(event.getSceneX());
+	        	((Node) event.getSource()).getTransforms().clear();
+	        	((Node) event.getSource()).getTransforms().add(new Rotate(angle/5 ,(((BorderPane)event.getSource()).getWidth())/2,((BorderPane)event.getSource()).getHeight()));
+	        	
 	        	
 	        }
 	    });
 	    
+	    this.setOnMouseReleased(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				if(((Recherche_profil)event.getSource()).dragged) {
+					((Recherche_profil)event.getSource()).dragged=false;
+					//((Node) event.getSource()).getTransforms().clear();
+					//RotateTransition transition = new RotateTransition(Duration.seconds(1),(Node)event.getSource());
+					//transition.X
+					//transition.setToAngle(90);
+				//	transition.setAxis(new Point3D(42,69,49));
+					//transition.play();
+					//transition
+				}
+				
+			}
+	    	
+	    });
+	    
+	    
+	    this.setOnMousePressed(new EventHandler<MouseEvent>() {
+
+			@Override
+			public void handle(MouseEvent event) {
+				
+					((Recherche_profil)event.getSource()).coordsX=(int)event.getX();
+					System.out.println(((Recherche_profil)event.getSource()).coordsX);
+				
+			
+			}
+	    });
+	    
+	    
+	    
+	    
 		/*
 	    BackgroundSize bSize = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, false);
 
-	    this.setBackground(new Background(new BackgroundImage(image2,
+	    this.setBackground(new Background(new BackgroundImage(imagecourante,
 	            BackgroundRepeat.NO_REPEAT,
 	            BackgroundRepeat.NO_REPEAT,
 	            BackgroundPosition.CENTER,
-	            bSize)));
-	    */
+	            bSize)));*/
+	    
 	}
 	
 	
